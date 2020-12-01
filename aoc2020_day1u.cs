ab46de7010bd7be2d819140f36b4ce9cea8f3488
pitchp <58635123+pitchp@users.noreply.github.com>
2020-12-01 22:26:52 +0700

    Update and rename aoc_day1u.cs to aoc2020_day1u.cs

--- aoc2020_day1u.cs
+++ aoc2020_day1u.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class aoc_day1u : MonoBehaviour {
+public class aoc2020_day1u : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
